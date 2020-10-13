55851709b780c376e8cf97a883f970686963ec3e
Sanjar <62173123+sange-klychev@users.noreply.github.com>
2020-10-13 17:55:14 +0300

    feat: added work-slider & skills bar

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,22 @@ $(() => {
         $('.bar').toggleClass('bar--active');
     });
 
+    $('.work__items').slick({
+        slideToShow: 1,
+        infinite: false,
+        prevArrow: '<button type="button" class="slick-prev icon-angle-left"></button>',
+        nextArrow: '<button type="button" class="slick-next icon-angle-right"></button>',
+        responsive: [
+            {
+                breakpoint: 500,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+
     wow = new WOW(
         {
             boxClass: 'wow',      // default
